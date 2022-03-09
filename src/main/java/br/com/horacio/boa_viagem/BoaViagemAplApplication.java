872dcadc52dc3be768784aff9f7f62d7e14a3fc9
Henrique <henrique@test.com>
2022-03-09 14:13:19 -0300

    Criação de Projeto

---
+++ src/main/java/br/com/horacio/boa_viagem/BoaViagemAplApplication.java
@@ -0,0 +1,13 @@
+package br.com.horacio.boa_viagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoaViagemAplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoaViagemAplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/horacio/boa_viagem/BoaViagemAplApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.horacio.boa_viagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoaViagemAplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
